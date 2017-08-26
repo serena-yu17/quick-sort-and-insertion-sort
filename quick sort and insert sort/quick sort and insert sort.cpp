b35ea845ce791e22777bc0eb06be51589e151578
Serena <serena.f.yu@gmail.com>
2017-08-26 21:52:23 +1000

    Bug fix
    Fixed bugs that messed up the sort.

--- quick sort and insert sort/quick sort and insert sort.cpp
+++ quick sort and insert sort/quick sort and insert sort.cpp
@@ -14,46 +14,57 @@ void swap_int(int*, int*);
 int *mid(int*, int*, int*);
 void insertsort(int*, int);
 
+
+vector<int> getint(string s) {
+	if (s.size() == 0)
+		return vector<int>();
+	string buffer;
+	vector<int> nums;
+	int i = 0;
+	while (i < s.size()) {
+		while (i < s.size() && (isdigit(s[i]) || s[i] == '-')) {
+			buffer.push_back(s[i]);
+			i++;
+		}
+		if (buffer.size() != 0) {
+			nums.push_back(stoi(buffer));
+			buffer.clear();
+		}
+		i++;
+	}
+	return nums;
+}
+
 int main()
 {
 	while (1)
 	{
 		cout << "Input your sequence:" << endl;
-		string raw, process;
+		string raw;
 		getline(cin, raw);
-		int temp;
-		stringstream parse_str(raw);
-		vector<int> numbers;
-		while (getline(parse_str, process, ' '))
-		{
-			if (stringstream(process) >> temp)
-				numbers.push_back(temp);
-		}
+		vector<int> numbers = getint(raw);
 		int size = numbers.size();
-		quicksort(&numbers[0], size);
+		int* arr = new int[size];
+		for (int i = 0; i < size; i++)
+			arr[i] = numbers[i];
+		quicksort(arr, size);
 		cout << "Quick sort:" << endl;
-		for (int a : numbers)
-			cout << a << ", ";
+		for (int i = 0; i < size; i++)
+			cout << arr[i] << " ";
 		cout << endl;
+		delete[] arr;
 	}
 }
 
 int *mid(int *num1, int *num2, int *num3)
 {
-	if (*num1 >= *num2)
-	{
-		if (*num1 <= *num3)
-			return num1;
-		else
-			return *num2 >= *num3 ? num2 : num3;
-	}
-	else
-	{
-		if (*num1 >= *num3)
-			return num1;
-		else
-			return *num2 <= *num3 ? num2 : num3;
-	}
+	if (*num1 > *num2)
+		swap_int(num1, num2);
+	if (*num1 > *num3)
+		swap_int(num1, num3);
+	if (*num2 > *num3)
+		swap_int(num2, num3);
+	return num2;
 }
 
 void swap_int(int *swap1, int *swap2)
@@ -64,7 +75,7 @@ void swap_int(int *swap1, int *swap2)
 	*swap2 = temp;
 }
 
-void quicksort(int * iarray, int size)
+void quicksort(int* iarray, int size)
 {
 	if (!(size > 1))
 		return;
@@ -78,12 +89,12 @@ void quicksort(int * iarray, int size)
 			return;
 		}
 	}
-	if (size < 8)
+	if (size < 10)
 	{
 		insertsort(iarray, size);
 		return;
 	}
-	int* pivot = mid(iarray, iarray + size - 1, iarray + (int)((size + 1) / 2));
+	int* pivot = mid(iarray, iarray + (int)((size + 1) / 2), iarray + size - 1);
 	swap_int(pivot, iarray + size - 1);
 	int i = 0, j = size - 1;
 	while (i < j)
@@ -93,13 +104,11 @@ void quicksort(int * iarray, int size)
 		while (iarray[--j] > iarray[size - 1])
 			;
 		if (i < j)
-		{
 			swap_int(iarray + i, iarray + j);
-		}
 	}
 	swap_int(iarray + i, iarray + size - 1);
 	quicksort(iarray, i);
-	quicksort(iarray + i, size - i);
+	quicksort(iarray + i + 1, size - i - 1);
 }
 
 void insertsort(int* iarray, int size)
